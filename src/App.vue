dde9fdf5094ed11cbb837f058ba894f6bd98e73d
ykpark <read0more@gmail.com>
2022-04-22 18:08:13 +0900

    Conduit template 적용(header, footer, home)

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import Header from "./components/Header.vue";
+import Footer from "./components/Footer.vue";
+import Home from "./pages/Home.vue";
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <Header />
+  <Home />
+  <Footer />
 </template>
 
 <style>
